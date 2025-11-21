af606e6389e3fe0010cb4c976e244d70236deefd
romolo <dyingforge@CHINAMI-ARERDTC.localdomain>
2025-11-21 18:19:18 +0800

    fix

--- src/suiNetworkConfig.ts
+++ src/suiNetworkConfig.ts
@@ -8,13 +8,13 @@ const { networkConfig, useNetworkVariable, useNetworkVariables } =
     },
     testnet: {
       url: getFullnodeUrl("testnet"),
-      packageId: "0xb1ef5fb760a44fc8783437985b4295dc8ac4db4a07a1b90c2fc01c53115cd347",
+      packageId: "0x00da184f41d1d9893ed82ae6d3bfbfa5e00d797bb3b6de8257a8967063d1a57d",
       surgeState:"0x197b67a3ba45276868494b666e60a26c69507d61c9af57956592112dc2efe908",
-      ACL:"0xa46311146098fcd702643c9cba4ff058bdd57abc8822ece5731ac5be7ecfce8a",
-      superAdmin:"0x98c6b66efc22158ec1a939c911c2fb1bee9f27cd81f39cd666c6405da15ebe02",
-      treasuryCap:"0xe4e71b7572a6cce6953ca783149044884928a0ef3fb6f316048cbfc92fa4f0ff",
+      ACL:"0xaca83e779928e52428cb75f4e7d9fae0389f4786687629f84d34d2a2c5d865cc",
+      superAdmin:"0x0f6a5914beaeea6e8f4f126aa5b3368493e02ab3a1d50f75d4be124b6709fe16",
+      treasuryCap:"0xaf516a53a6d6773574bd8a9ce4655ce6d895711859a587d249d9d96a5fed76ae",
       state:"0x31358d198147da50db32eda2562951d53973a0c0ad5ed738e9b17d88b213d790",
-      bridgeState:"0x61d9dbd5b4091ca45c9ede2dbcb1bd88469e870f1aaa2de2d9ce98036519b43f"
+      bridgeState:"0x205a2a17a6c2dea637b05af597cbd4eeaa61ad8d1db7bb61bbf8e63cacce41fc"
     },
     mainnet: {
       url: getFullnodeUrl("mainnet"),
